45caaf465f8e588c6e5a224146acd4babbb33606
Nicole Sad <nsad308@gmail.com>
2017-10-11 12:45:12 -0600

    Starting the new Move Script

---
+++ Ghost House/Assets/Scripts/Player_Move.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Move : MonoBehaviour {
+	public float moveSpeed;
+	public float turnSpeed;
+	public float jumpHeight;
+
+	void Update () {
+	var z = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+	var y = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
+	var j = Input.GetAxis("Jump") * Time.deltaTime * jumpHeight;
+
+
+	transform.Translate(0,0,z);
+	transform.Rotate(0,y,0);
+	transform.Translate(0,j,0);
+	}
+}
