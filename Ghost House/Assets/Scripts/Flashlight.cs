6cb3bd682763a7f924888205c045202f09b9555c
Nicole Sad <nsad308@gmail.com>
2017-10-25 13:11:56 -0600

    Adding Lights and enabling flashlight

---
+++ Ghost House/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Flashlight : MonoBehaviour {
+
+	public bool lightOn;
+
+	Light light;
+
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light> ();
+		//Set Light default to On
+		lightOn = true;
+		light.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Toggle light on/off when F key is pressed
+		if (Input.GetKeyUp )KeyCode.F) && lightOn) {
+			lightOn = false;
+			light.enabled = false;
+		}
+
+		else if (Input.GetKeyUp (KeyCode.F) && !lightOn){
+			lightOn = true;
+			light.enabled = true;
+
+		}
+	
+	}
+}
